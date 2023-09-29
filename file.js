6ffec9e5bae4bb71807e52085c8129ed29a716d0
Marissa Burca <Marissaburca@gmail.com>
2023-09-29 19:11:17 +0200

    esteticamente ben fatto. Js inattivo

--- file.js
+++ file.js
@@ -1,8 +1,8 @@
-const motoUp = document.getElementById('capo')
-motoUp.addEventListener('scrollY',(y) => { if (y>410){
-    motoUp.classList.add('blanc')
-}else{
-    motoUp.removeClass('blanc')
-}
-
+const motoUp = document.getElementById("capo")
+motoUp.addEventListener("scrollY", (y) => {
+  if (y > 410) {
+    motoUp.classList.add("blanc");
+  } else {
+    motoUp.removeClass("blanc");
+  }
 })

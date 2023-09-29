90a96a388eed50b91b64036a250ce3b58d192a6a
Marissa Burca <Marissaburca@gmail.com>
2023-09-29 18:23:14 +0200

    ingombro testo non sborda, js ancora inattivo

--- file.js
+++ file.js
@@ -1,6 +1,8 @@
 const motoUp = document.getElementById('capo')
 motoUp.addEventListener('scrollY',(y) => { if (y>410){
     motoUp.classList.add('blanc')
+}else{
+    motoUp.removeClass('blanc')
 }
 
 })

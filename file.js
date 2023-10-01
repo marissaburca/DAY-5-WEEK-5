9b54a6215b2d9c644d317384bcae2cc7fad62c99
Marissa Burca <Marissaburca@gmail.com>
2023-10-02 01:08:54 +0200

    js funzionante

--- file.js
+++ file.js
@@ -13,10 +13,9 @@ window.onscroll = function(){
 
 
 
-
-const emme = () => {
-  const elements0 = document.querySelectorAll(".mS > svg > g > g > g[opacity='0']");
-  const elements1 = document.querySelectorAll(".mS > svg > g > g > g[opacity='1']");
+/* const emme = () => {
+  const elements0 = document.querySelectorAll("svg > g > g > g[opacity='0']");
+  const elements1 = document.querySelectorAll("svg > g > g > g[opacity='1']");
 
   let rnd = Math.floor(Math.random() * elements0.length);
   elements0[rnd].setAttribute("opacity", "1");
@@ -24,4 +23,35 @@ const emme = () => {
   rnd = Math.floor(Math.random() * elements1.length);
   elements1[rnd].setAttribute("opacity", "0");
 };
+
+setInterval(emme, 200);
+ */
+const emme = () => {
+  const elements = Array.from(document.querySelectorAll("svg > g > g > g[opacity='1']"));
+  const shuffledElements = elements.sort(() => Math.random() - 0.5);
+  const selectedElements = shuffledElements.slice(0, 70);
+
+  anime({
+    targets: selectedElements,
+    opacity: [1, 0],
+    duration: 3000,
+    delay: anime.stagger(3000 / selectedElements.length),
+    
+    direction: "normal",
+    loop: false
+  });
+
+  anime({
+    targets: elements.filter((element) => !selectedElements.includes(element)),
+    opacity: [0, 1],
+    duration: 4000,
+    delay: anime.stagger(1000 / (elements.length - selectedElements.length)),
+   
+    direction: "normal",
+    loop: true
+  });
+};
+
+setInterval(emme, 100);
+
 

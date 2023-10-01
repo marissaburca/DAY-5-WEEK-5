b5a82e8ea4aea0a2479a9e85dd536a8f1deb0aa7
Marissa Burca <Marissaburca@gmail.com>
2023-10-01 21:40:33 +0200

    m JS absent

--- file.js
+++ file.js
@@ -14,12 +14,14 @@ window.onscroll = function(){
 
 
 
-const arr= []
-function extractRandomGElementsFromSVG() {
-  const svg = document.querySelector('svg.caos');
-  const gElements = svg.querySelectorAll('g');
-  const randomIndex = Math.floor(Math.random() * gElements.length);
-  returnarr.push(gElements[randomIndex]);
-}
-console.log(extractRandomGElementsFromSVG())
-const appears = ()=>{}
+const emme = () => {
+  const elements0 = document.querySelectorAll(".mS > svg > g > g > g[opacity='0']");
+  const elements1 = document.querySelectorAll(".mS > svg > g > g > g[opacity='1']");
+
+  let rnd = Math.floor(Math.random() * elements0.length);
+  elements0[rnd].setAttribute("opacity", "1");
+
+  rnd = Math.floor(Math.random() * elements1.length);
+  elements1[rnd].setAttribute("opacity", "0");
+};
+
